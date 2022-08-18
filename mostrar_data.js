8a8c97a3bacab6c9b02752e7d3b64d6d423f4a6f
Gilbson-Arandas <109983801+Gilbson-Arandas@users.noreply.github.com>
2022-08-17 22:59:59 -0300

    Update mostrar_data.js

--- mostrar_data.js
+++ mostrar_data.js
@@ -1,1 +1,3 @@
 <script type="text/javascript">document.write(Date());</script>
+
+// Minha escolha para usar como editor de texto para programa em JavaScript foi o VSCODE pois além de ser gratuito ele também é open source
